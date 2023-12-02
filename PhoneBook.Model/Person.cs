8a68a9a931fcb5402ed77f3b5c8301524ce6ce77
yury <shubin1978@omnicommdv.ru>
2023-12-02 13:20:04 +0800

    Добавление модели данных

---
+++ PhoneBook.Model/Person.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PhoneBook.Model;
+
+//public record Person(int Id, string LastName, string? FirstName, List<Phone> Phones);
+[Table("table_persons")]
+public record Person
+{
+    [Column("id")]
+    [Required]
+    public int Id { get; set; }
+    
+    [Column("last_name")]
+    [Required]
+    public string LastName { get; set; }
+    
+    [Column("first_name")]
+    public string? FirstName { get; set; }
+    
+    private IEnumerable<Phone> Phones { get; set; }
+}
---
+++ PhoneBook.Model/Phone.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace PhoneBook.Model;
+
+public enum PhoneType
+{
+    Unknown, Mobile, Home
+}
+
+[Table("table_phones")]
+[Index(nameof(Number), IsUnique = true, Name = "phone_number_index")]
+public record Phone
+{
+    [Column("id")]
+    [Required]
+    public int Id { get; set; }
+    
+    [Column("phone_type")]
+    [Required]
+    public PhoneType Type { get; set; }
+    
+    [Column("number")]
+    [Required]
+    public string Number { get; set; }
+    
+    [Column("person_id")]
+    [Required]
+    public int PersonId { get; set; }
+    [ForeignKey(nameof(PersonId))]
+    public Person Person { get; set; }
+};
